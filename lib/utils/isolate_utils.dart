0e68fe6af759da4c25f5aace4df6dd1d70f2df6f
Lê Sang <42503707+LeSangDeveloper@users.noreply.github.com>
2023-01-04 15:12:19 +0800

    init isolate utils class

---
+++ lib/utils/isolate_utils.dart
@@ -0,0 +1,35 @@
+import 'dart:isolate';
+
+import 'package:camera/camera.dart';
+
+class IsolateUtils {
+  static const String DEBUG_NAME = "InferenceIsolate";
+
+  late Isolate _isolate;
+  final ReceivePort _receivePort = ReceivePort();
+  late SendPort _sendPort;
+
+  Future<void> startAsync() async {
+    _isolate = await Isolate.spawn<SendPort>(entryPointAsync, _receivePort.sendPort, debugName: DEBUG_NAME);
+
+    _sendPort = await _receivePort.first;
+  }
+
+  static Future<void> entryPointAsync(SendPort sendPort) async {
+    final port = ReceivePort();
+    sendPort.send(port);
+
+    await for (final IsolateData isolateData in port) {
+
+    }
+  }
+
+}
+
+class IsolateData {
+  CameraImage cameraImage;
+  int interpreterAddress;
+  List<String> labels;
+
+  IsolateData(this.cameraImage, this.interpreterAddress, this.labels);
+}
